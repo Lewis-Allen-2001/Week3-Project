148e4d3e6ba8d2c5caf1c1980f23c885a08c5951
Lewis-Allen-2001 <itslewbie01@gmail.com>
2024-08-31 14:33:49 +0100

    nearly done!

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 let cookieCount = 0;
 let cookiesPerSecond = 1;
+let autoClickerPrice = 100;
+let enhancedOvenPrice = 500;
+let cookieFarmPrice = 1000;
+let robotBakerPrice = 2000;
+let cookieFactoryPrice = 5000;
+let magicFlowerPrice = 10000;
+let timeMachinePrice = 20000;
+let quantumOvenPrice = 50000;
+let alienTechnologyPrice = 100000;
+let interdimensionalBakerPrice = 200000;
 
 const cookieCountDisplay = document.getElementById("cookieCount");
 const cookiesPerSecondDisplay = document.getElementById("cookiesPerSecond");
@@ -54,4 +64,114 @@ function displayUpgrades(arrayofUpgrades) {
   arrayofUpgrades.forEach(function (upgrade) {
     console.log(upgrade);
   });
+}
+
+document.getElementById("autoClicker").addEventListener("click", function () {
+  if (cookieCount >= autoClickerPrice) {
+    cookieCount -= autoClickerPrice;
+
+    cookiesPerSecond += 1;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document.getElementById("Enhanced Oven").addEventListener("click", function () {
+  if (cookieCount >= enhancedOvenPrice) {
+    cookieCount -= enhancedOvenPrice;
+
+    cookiesPerSecond += 5;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document.getElementById("Cookie Farm").addEventListener("click", function () {
+  if (cookieCount >= cookieFarmPrice) {
+    cookieCount -= cookieFarmPrice;
+
+    cookiesPerSecond += 10;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document.getElementById("Robot Baker").addEventListener("click", function () {
+  if (cookieCount >= robotBakerPrice) {
+    cookieCount -= robotBakerPrice;
+
+    cookiesPerSecond += 20;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document
+  .getElementById("Cookie Factory")
+  .addEventListener("click", function () {
+    if (cookieCount >= cookieFactoryPrice) {
+      cookieCount -= cookieFactoryPrice;
+
+      cookiesPerSecond += 50;
+    } else {
+      alert("Slow down cowboy , you dont have enough cookies!");
+    }
+  });
+
+document.getElementById("Magic Flower").addEventListener("click", function () {
+  if (cookieCount >= magicFlowerPrice) {
+    cookieCount -= magicFlowerPrice;
+
+    cookiesPerSecond += 100;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document.getElementById("Time Machine").addEventListener("click", function () {
+  if (cookieCount >= timeMachinePrice) {
+    cookieCount -= timeMachinePrice;
+
+    cookiesPerSecond += 200;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document.getElementById("Quantum Oven").addEventListener("click", function () {
+  if (cookieCount >= quantumOvenPrice) {
+    cookieCount -= quantumOvenPrice;
+
+    cookiesPerSecond += 500;
+  } else {
+    alert("Slow down cowboy , you dont have enough cookies!");
+  }
+});
+
+document
+  .getElementById("Alien Technology")
+  .addEventListener("click", function () {
+    if (cookieCount >= alienTechnologyPrice) {
+      cookieCount -= alienTechnologyPrice;
+
+      cookiesPerSecond += 1000;
+    } else {
+      alert("Slow down cowboy , you dont have enough cookies!");
+    }
+  });
+
+document
+  .getElementById("Interdimensional Baker")
+  .addEventListener("click", function () {
+    if (cookieCount >= interdimensionalBakerPrice) {
+      cookieCount -= interdimensionalBakerPrice;
+
+      cookiesPerSecond += 2000;
+    } else {
+      alert("Slow down cowboy , you dont have enough cookies!");
+    }
+  });
+
+if (cookieCount >= 1000000) {
+  alert("WOW ,You are mega rich!");
 }
